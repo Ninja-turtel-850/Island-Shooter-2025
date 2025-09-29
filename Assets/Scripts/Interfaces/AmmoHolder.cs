bc316079a39f3ab7c5ee84694eff0ae4cf4bcb5b
Justiniuz <Justin.nijenhuis@gmail.com>
2025-09-29 10:53:03 +0200

    voor mijn schatje
    hier tygo

--- Assets/Scripts/Interfaces/AmmoHolder.cs
+++
@@ -1,6 +0,0 @@
-public interface IAmmoHolder
-{
-    int GetAmmo(BulletType gunType);
-    void RemoveAmmo(BulletType gunType, int amount);
-    void AddAmmo(BulletType gunType, int amount);
-}
--- Assets/Scripts/Interfaces/Pickupable.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface IPickupable
-{
-    void Pickup(Transform transform);
-}
